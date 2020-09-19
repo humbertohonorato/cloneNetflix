9bb9c098c9576f79d19c8be21a86de35ca941082
humberto <humberto.honorato@outlook.com>
2020-09-19 17:04:08 -0300

    commit

--- rabiflix/src/components/Header.js
+++ rabiflix/src/components/Header.js
@@ -6,7 +6,8 @@ export default ({ black }) => {
         <header className={black ? 'black' : ''}>
             <div className="header--logo">
                 <a href="/">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="Netflix"></img>
+                    <h1>RABFLIX</h1>
+                    {/* <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="Netflix"></img> */}
                 </a>
             </div>
             <div className="header--user">
